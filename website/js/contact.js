020f6e734774bc6d5ff14608b70cc36b43380ba2
Alexandra Havia <sasha.havia@gmail.com>
2015-09-14 16:25:06 -0400

    Finished contact Page

--- website/js/contact.js
+++ website/js/contact.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     var latlng = new google.maps.LatLng(40.645244,-73.9449976);
-    console.log("I am here");
+    //console.log("I am here");
     var styles = [
         {
             featureType: "landscape",
@@ -35,12 +35,81 @@ $(document).ready(function () {
                     ]
                 }
             ];
-            var myOptions = {
-                zoom: 10,
-                center: latlng,
-                mapTypeId: google.maps.MapTypeId.ROADMAP,
-                disableDefaultUI: true,
-                styles: styles
-            };
-            map = new google.maps.Map(document.getElementById('map'), myOptions);
+        var myOptions = {
+            zoom: 10,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            disableDefaultUI: true,
+            styles: styles
+        };
+        map = new google.maps.Map(document.getElementById('map'), myOptions);
+
+// Form Validation
+
+    var email = "";
+    var name = "";
+    var subject = "";
+    var message = "";
+    $("#submit").click(function(){
+        //console.log("CLICK WORKS");
+        name = $("#name").val();
+        email = $("#email").val();
+        
+        console.log($('input[name="topic"]:checked').serialize());
+
+        $('input[name="topic"]:checked').each(function() {
+           console.log(this.value); 
+           subject = $(this).val();
+           console.log(subject); 
+        });
+
+        emailValid = validate( "email", email );
+
+        if( emailValid ){
+        }
+    });
+
+    function validate(dataType, data){
+        var result = false;
+
+        if(data != ""){
+            //console.log("Continue Validation");
+            switch(dataType){
+                case "email":
+                    //validate email
+                    //find @
+                    if(data.indexOf('@') != -1){
+                        //console.log("The @ found");
+                        result = true;
+                        if(data.indexOf('.') != -1){
+                            //console.log("valid");
+                            result = true;
+                        }else{
+                            //console.log("inavlid email");
+                            result = false;
+                        }
+                    }else{
+                        //console.log(" No @ was found, invalid email");
+                        result = false;
+                    }
+                    break;
+                case "name":
+                    //validate password
+                    if(data.length >= 6){
+                        //valid
+                        result = true;
+                    }else{
+                        //valid
+                        result = false;
+                    }
+                    break;
+            }
+        }else{
+            //console.log( "No data provided!");
+            result = false;
+        }
+        return result;
+    }
+
+
 });
--- website/js/proposal.js
+++ website/js/proposal.js
@@ -1,25 +1,51 @@
 $(document).ready(function(){
-	console.log("proposal");
+	//console.log("proposal");
 	var firstChoice = "";
-	
-	console.log("This is first Choice "+ firstChoice);
+	var webChoice = "";
+	var storeChoice = "";
+	var wordChoice = "";
+	//console.log("This is first Choice "+ webFirstChoice);
 
+	// Project Type selection
 	$("#project_type").on("change", function(){
-		fChoice = $(this).val();
-		console.log(fChoice);
+		firstChoice = $(this).val();
+		//console.log(webFirstChoice);
 		checkValue();
-
 		//checkValue( $(this).val() );
 	});
 
 	function checkValue(){
-			if( fChoice == "type1" ){
-					console.log("correct");	
-					$("#choice2").show();
-			}else{
-				//Game end
-			}
+		if( firstChoice == "type1" ){
+			//console.log("correct");	
+			$("#choice2").show();
+		}else if( firstChoice == "type2" ){
+			$("#choice3").show();
+		}else if( firstChoice == "type3" ){
+			$("#choice4").show();
+		}
 		
+	}
+	//
+	$("#project_pages").on("change", function(){
+		webChoice = $(this).val();
+		checkValueWeb(webChoice);
+	});
+
+	$("#project_products").on("change", function(){
+		storeChoice = $(this).val();
+		checkValueWeb(storeChoice);
+	});
+
+	$("#project_word").on("change", function(){
+		wordChoice = $(this).val();
+		checkValueWeb(wordChoice);
+	});
+
+	function checkValueWeb(data){
+		if(data != ""){
+			$("#choice5").show();
+		}
+
 	}
 
 	
